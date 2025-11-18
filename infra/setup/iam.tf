108d97df65f101245da060373cb9832daa770866
kubousky <kparcheta@gmail.com>
2025-11-18 19:19:28 +0100

    add-rds-instance

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -133,6 +133,7 @@ data "aws_iam_policy_document" "ec2" {
       "ec2:CreateInternetGateway",
       "ec2:AttachInternetGateway",
       "ec2:ModifyVpcAttribute",
+      "ec2:RevokeSecurityGroupIngress",
     ]
     resources = ["*"]
   }
@@ -168,6 +169,19 @@ data "aws_iam_policy_document" "rds" {
       "rds:AddTagsToResource",
     ]
     resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreateServiceLinkedRole",
+    ]
+    resources = ["arn:aws:iam::*:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS"]
+    condition {
+      test     = "StringLike"
+      variable = "iam:AWSServiceName"
+      values   = ["rds.amazonaws.com"]
+    }
   }
 }
 
