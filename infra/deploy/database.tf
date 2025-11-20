21643a873e3f395e7b91dd2fdf4f83927f5cf66c
kubousky <kparcheta@gmail.com>
2025-11-20 23:49:26 +0100

    creating db

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "rds" {
 resource "aws_db_instance" "main" {
   identifier                 = "${local.prefix}-db"
   db_name                    = "recipe"
-  allocated_storage          = 2
+  allocated_storage          = 20
   storage_type               = "gp2"
   engine                     = "postgres"
   engine_version             = "15"
