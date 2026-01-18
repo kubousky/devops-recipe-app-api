21fbd9ff529b4392955474e46ee5eb0e233b3a6f
kubousky <kparcheta@gmail.com>
2026-01-18 18:17:54 +0100

    update lb

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -30,21 +30,21 @@ resource "aws_security_group" "lb" {
 }
 
 resource "aws_lb" "api" {
-    name = "${local.prefix}-lb"
-    load_balancer_type = "application"
-    subnets = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-    security_groups = [aws_security_group.lb.id]
+  name               = "${local.prefix}-lb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  security_groups    = [aws_security_group.lb.id]
 }
 
 resource "aws_lb_target_group" "api" {
-  name     = "${local.prefix}-api"
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "${local.prefix}-api"
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
-  port     = 8000
+  port        = 8000
 
   health_check {
-    path                = "/api/health-check/"
+    path = "/api/health-check/"
   }
 }
 
